553ee713753e0a5a9248275cf31391e66779c8b2
Petr Blaho <petrblaho@gmail.com>
2026-08-24 17:31:18 +0200

    deps: add oban and uniq for background jobs and uuidv7

--- mix.exs
+++ mix.exs
@@ -81,6 +81,8 @@ defmodule Schnur.MixProject do
        depth: 1},
       {:swoosh, "~> 1.16"},
       {:req, "~> 0.5"},
+      {:oban, "~> 2.23"},
+      {:uniq, "~> 0.6"},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 1.0"},
