584f561900eef16f9a16bf377cfc1821126e031e
Petr Blaho <petrblaho@gmail.com>
2026-08-24 16:54:28 +0200

    ci: Probe compile gate with intentional warning

--- lib/schnur.ex
+++ lib/schnur.ex
@@ -6,4 +6,10 @@ defmodule Schnur do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  @doc false
+  def _warning_probe do
+    unused = 1
+    :ok
+  end
 end
